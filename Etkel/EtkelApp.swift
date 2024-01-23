6e038ec0e3162a316535da057213a79b967534d2
Amal-Alhamad <amalhamad1419@gmail.com>
2024-01-23 16:52:57 +0300

    Initial Commit

---
+++ Etkel/EtkelApp.swift
@@ -0,0 +1,17 @@
+//
+//  EtkelApp.swift
+//  Etkel
+//
+//  Created by Amal Saad on 11/07/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct EtkelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
